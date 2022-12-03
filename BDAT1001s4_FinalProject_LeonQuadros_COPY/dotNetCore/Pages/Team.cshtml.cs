e82feae06dfb3c3258b55e3b30a8361bcb347590
200532151-LeonQuadros <200532151@student.georgianc.on.ca>
2022-12-02 23:34:50 -0500

    FinalProjectIES_Group10

---
+++ BDAT1001s4_FinalProject_LeonQuadros_COPY/dotNetCore/Pages/Team.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactManager.Pages;
+
+[AllowAnonymous]
+public class TeamModel : PageModel
+{
+    private readonly ILogger<TeamModel> _logger;
+
+    public TeamModel(ILogger<TeamModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
